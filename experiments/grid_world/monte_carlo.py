529e47aef0108eef2b5bad367af8a173978bfeb1
cc299792458 <cc2018011855@gmail.com>
2024-08-23 03:58:33 -0400

    monte carlo gridworld debug

--- experiments/grid_world/monte_carlo.py
+++ experiments/grid_world/monte_carlo.py
@@ -11,33 +11,33 @@ from traditional_algos.monte_carlo import MonteCarlo
 if __name__ == '__main__':
     set_seed()
     ##### Step 0: Build GridWorld environment and initiate policy #####
-    env = GridWorld(grid_size=3)
+    env = GridWorld(grid_size=4)
     agent = MonteCarlo(env=env, epsilon=0.0)
     num_episodes = [1, 10, 50, 100, 200, 500, 1000, 5000, 10000, 50_000, 500_000]
 
-    # # ##### Step 1: Try monte carlo prediction to estimate the value function #####
-    # for num_episode in num_episodes:
-    #     start_time = time.time()
-    #     agent.prediction(num_episode)
-    #     end_time = time.time()
-    #     print(f"-----Monte carlo prediction with {num_episode} episode-----")
-    #     print(f"Time: {end_time-start_time}")
-    #     agent.print_value_function()
-    #     print("\n")
+    # ##### Step 1: Try monte carlo prediction to estimate the value function #####
+    for num_episode in num_episodes:
+        start_time = time.time()
+        agent.prediction(num_episode)
+        end_time = time.time()
+        print(f"-----Monte carlo prediction with {num_episode} episode-----")
+        print(f"Time: {end_time-start_time}")
+        agent.print_value_function()
+        print("\n")
     
-    # ##### Step 2: Try monte carlo estimation and control to estimate the q function and optimal policy pi #####
-    # for num_episode in num_episodes:
-    #     start_time = time.time()
-    #     agent.estimation_and_control(num_episode)
-    #     end_time = time.time()
-    #     print(f"-----Monte carlo estimation and control with {num_episode} episode-----")
-    #     print(f"Time: {end_time-start_time}")
-    #     agent.print_q_function()
-    #     agent.print_policy()
-    #     print("\n")
+    ##### Step 2: Try monte carlo estimation and control to estimate the q function and optimal policy pi #####
+    for num_episode in num_episodes:
+        start_time = time.time()
+        agent.estimation_and_control(num_episode)
+        end_time = time.time()
+        print(f"-----Monte carlo estimation and control with {num_episode} episode-----")
+        print(f"Time: {end_time-start_time}")
+        agent.print_q_function()
+        agent.print_policy()
+        print("\n")
 
     ##### Step 3: Try monte carlo estimation and control with epsilon greedy #####
-    agemt = MonteCarlo(env=env, epsilon=0.1)
+    agent = MonteCarlo(env=env, epsilon=0.1)
     for num_episode in num_episodes:
         start_time = time.time()
         agent.estimation_and_control(num_episode)
