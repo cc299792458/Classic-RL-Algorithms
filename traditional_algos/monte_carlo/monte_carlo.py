c8e238e9c663c3fd851b2ed4fd796859d3830fab
cc299792458 <cc2018011855@gmail.com>
2024-08-25 02:53:49 -0400

    every-visit monte carlo

--- traditional_algos/monte_carlo/monte_carlo.py
+++ traditional_algos/monte_carlo/monte_carlo.py
@@ -9,10 +9,11 @@ from tqdm import tqdm
 from utils.gym_utils import get_observation_shape
 
 class MonteCarlo:
-    def __init__(self, env: gym.Env, gamma=1.0, epsilon=0.1, initial_policy=None) -> None:
+    def __init__(self, env: gym.Env, gamma=1.0, epsilon=0.1, initial_policy=None, first_visit=True) -> None:
         self.env = env
         self.gamma = gamma
         self.epsilon = epsilon
+        self.first_visit = first_visit
         self.observation_shape = get_observation_shape(env.observation_space)
         self.num_action = self.env.action_space.n
 
@@ -20,15 +21,15 @@ class MonteCarlo:
         self.reset()
 
     def reset(self):
-        """
-            Reset the policy to a uniform random policy and reset the value and Q functions.
-        """
+        """Reset the policy to a uniform random policy and reset the value and Q functions."""
         if self.initial_policy is None:
             self.policy = np.ones((*self.observation_shape, self.num_action)) / self.num_action
         else:
             self.policy = self.initial_policy
+
         self.value_function = np.zeros(self.observation_shape)
         self.Q = np.zeros((*self.observation_shape, self.num_action))  # Action-value function
+
         # For incremental update
         self.state_counts = np.zeros(self.observation_shape)
         self.action_counts = np.zeros((*self.observation_shape, self.num_action))
@@ -37,23 +38,16 @@ class MonteCarlo:
         # self.returns_q = {state: {action: [] for action in range(self.num_action)} for state in np.ndindex(self.observation_shape)}
 
     def prediction(self, num_episode):
-        """
-            First-vist monte-carlo prediction, for estimating value function.
-            P.S, the name comes from chapter 5.1 of the RL book, monte carlo prediction
-        """
+        """Monte Carlo prediction for estimating the value function."""
         self.reset()
         with tqdm(total=num_episode) as pbar:
             for _ in range(num_episode):
                 episode = self.generate_episode()
                 self.update_value_function(episode)
                 pbar.update(1)
-    
+
     def estimation_and_control(self, num_episode):
-        """
-            First-visit monte-carlo esimation, for estimating Q function and policy pi.
-            P.S, the name comes from chapter 5.2 and 5.3 of the RL book, 
-            monte carlo esimation and monte carlo control
-        """
+        """Monte Carlo estimation for estimating the Q function and policy."""
         self.reset()
         with tqdm(total=num_episode) as pbar:
             for _ in range(num_episode):
@@ -63,29 +57,28 @@ class MonteCarlo:
                 pbar.update(1)
 
     def generate_episode(self):
-        """
-            Generate an episode following the current policy.
-        """
+        """Generate an episode following the current policy."""
         episode = []
         state, info = self.env.reset()
         done = False
+
         while not done:
             action = np.random.choice(np.arange(self.num_action), p=self.policy[state])
             next_state, reward, terminated, truncated, info = self.env.step(action=action)
             done = terminated or truncated
             episode.append((state, action, reward))
             state = next_state
+
         return episode
-    
+
     def update_value_function(self, episode):
-        """
-        Incrementally update the value function based on the generated episode.
-        """
+        """Incrementally update the value function based on the generated episode."""
         G = 0
+
         for state, action, reward in reversed(episode):
             G = self.gamma * G + reward
             
-            if not any(state == x[0] for x in episode[:episode.index((state, action, reward))]):
+            if (self.first_visit and not any(state==x[0] for x in episode[:episode.index((state, action, reward))])) or not self.first_visit:
                 # Increment the visit count
                 self.state_counts[state] += 1
                 
@@ -96,14 +89,13 @@ class MonteCarlo:
                 self.value_function[state] += alpha * (G - self.value_function[state])
 
     def update_q_function(self, episode):
-        """
-        Incrementally update the Q function based on the generated episode.
-        """
+        """Incrementally update the Q function based on the generated episode."""
         G = 0
+
         for state, action, reward in reversed(episode):
             G = self.gamma * G + reward
             
-            if not any(state==x[0] and action==x[1] for x in episode[:episode.index((state, action, reward))]):
+            if (self.first_visit and not any(state==x[0] and action==x[1] for x in episode[:episode.index((state, action, reward))])) or not self.first_visit:
                 # Increment the visit count
                 self.action_counts[state][action] += 1
                 
@@ -112,7 +104,7 @@ class MonteCarlo:
                 
                 # Incremental update of Q(s, a)
                 self.Q[state][action] += alpha * (G - self.Q[state][action])
-    
+
     # def update_value_function(self, episode):
     #     """
     #         Update the value function based on the generated episode.
@@ -142,11 +134,10 @@ class MonteCarlo:
     #             self.Q[state][action] = np.mean(self.returns_q[state][action])
 
     def improve_policy(self):
-        tolerance = 1e-8    # This is a too small tolerance
+        """Update the policy based on the updated Q function."""
+        tolerance = 1e-8  # A small tolerance value for comparisons
+
         for state in np.ndindex(self.observation_shape):
-            # if hasattr(self.env, 'is_terminal_state') and self.env.is_terminal_state(state):
-            #     continue  # Skip terminal states
-            # Find the best actions with Q-values close to the maximum
             max_q_value = np.max(self.Q[state])
             best_actions = np.argwhere(np.abs(self.Q[state] - max_q_value) <= tolerance).flatten()
 
@@ -159,19 +150,27 @@ class MonteCarlo:
                 self.policy[state] = (1 - self.epsilon) * self.policy[state] + (self.epsilon / self.num_action)
 
     def print_value_function(self):
+        """Print the current value function."""
         if hasattr(self.env, '_print_value_function'):
             self.env._print_value_function(self.value_function)
         else:
             print(self.value_function)
 
     def print_q_function(self):
+        """Print the current Q function."""
         if hasattr(self.env, '_print_q_function'):
             self.env._print_q_function(self.Q)
         else:
             print(self.Q)
 
     def print_policy(self):
+        """Print the current policy."""
         if hasattr(self.env, '_print_policy'):
             self.env._print_policy(self.policy)
         else:
             print(self.policy)
+
+
+class OffPolicyMonteCarlo(MonteCarlo):
+    """Implement Off-Policy Monte Carlo via Importance Sampling."""
+    pass
